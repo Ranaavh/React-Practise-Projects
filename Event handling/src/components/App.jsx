d7ec2cec71db6d8b4dcce7260e1a68245becf922
RANA <ranavh003@gmail.com>
2024-06-26 23:15:38 +0530

    Event Handling

--- Event handling/src/components/App.jsx
+++ Event handling/src/components/App.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 
-var userIsRegistered = true; // set false to see Confirm Password and Register button
-
-
-
-
-
-
-
 function App() {
+  const [heading, setHeading] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeading("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <Form isRegistered={userIsRegistered}/>
+      <form>
+        <h1>{heading}</h1>
+        <Form />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+      
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- Event handling/src/components/Form.jsx
+++ Event handling/src/components/Form.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-function Form(props) {
+function Form() {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      {!props.isRegistered&& (
-        <input type="password" placeholder="Confirm Password" />
-      )}
-      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+     
     </form>
   );
 }
