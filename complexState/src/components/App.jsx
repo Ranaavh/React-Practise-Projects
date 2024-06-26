7bfb0e968142643ba25c579a9f318ad312248ad9
RANA <ranavh003@gmail.com>
2024-06-26 23:56:53 +0530

    user form

--- complexState/src/components/App.jsx
+++ complexState/src/components/App.jsx
@@ -1,18 +1,65 @@
-import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = true; // set false to see Confirm Password and Register button
-
-
-
-
-
-
+import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <Form isRegistered={userIsRegistered}/>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          value={contact.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          value={contact.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          value={contact.email}
+          name="email"
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
